07de722b4a3663a371f72908d5f63c7c7ad5dd1e
soumyasrishti01 <78192188+soumyasrishti01@users.noreply.github.com>
2021-07-30 15:50:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,6 +8,20 @@ function setup() {
     video = createCapture(VIDEO);
     video.size(500, 500)
     video.center();
+
+    poseNet = ml5.poseNet(video, modleLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modleLoaded(){
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results){
+    if(results.length > 0) {
+        console.log("nose x =" + results[0].pose.nose.x);
+        console.log("nose y =" + results[0].pose.nose.y);
+    }
 }
 
 function draw(){
